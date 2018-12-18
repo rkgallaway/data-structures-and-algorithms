8bc22ade762bd35f1a81f6de4eee262cb70f9044
rkgallaway <rkgallaway@msn.com>
2018-12-17 16:22:49 -0800

    code challenge 01 finsihed and tested in codepen

--- arrayReverse/array-reverse.js
+++ arrayReverse/array-reverse.js
@@ -1,2 +1,15 @@
 'use strict';
 
+function reverseArray(inArray){
+  if(inArray.length <= 1){
+    return inArray;
+  }
+  let newArray = [];
+  let counter = 0;
+  for (let i = inArray.length; i <= 0; i--){
+    newArray[counter] = inArray[i];
+    counter++;
+  };
+  console.log(newArray);
+  return newArray;
+}
