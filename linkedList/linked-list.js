090c423a00bb0b720fc27ad7a45f8c44e58fa249
rkgallaway <rkgallaway@msn.com>
2018-12-27 00:43:53 -0800

    initial commit

---
+++ linkedList/linked-list.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//create node class
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  };
+};
+
+//created linked list class
+class LinkedList{
+  constructor(){
+    this.head = null;
+    this.next = null; // point to next??? or so the following methods handle that?
+	};
+	//define method insert here?
+	//define method includes here?
+	//define method print here?
+
+
+};
