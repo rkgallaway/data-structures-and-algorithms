0170d2fd57a2f091606f8a40b11e37e6850628c0
rkgallaway <rkgallaway@msn.com>
2018-12-27 23:36:15 -0800

    code challenge 2 fixes

--- arrayShift/array-shift.js
+++ arrayShift/array-shift.js
@@ -1,20 +1,23 @@
 'use strict';
 
-function insertShiftArray(inputArray, value){
+let arrayShift = module.exports = {};
+
+
+arrayShift.insertShiftArray = (inputArray, value) =>{
   let newArray = [];
-  let half = Math.ceil(inputArray.length / 2);
-  for( let i in inputArray){
-    if (i < half){
+  let half = inputArray.length;
+  for( let i = 0; i < half + 1; i++){
+    if (i < Math.ceil(half / 2)){
       newArray[i] = inputArray[i];
     }
-    if (i === half){
+    if (i === Math.ceil(half / 2)){
       newArray[i] = value;
     }
-    if (i > half){
-      newArray[i + 1] = inputArray[i];
+    if (i > Math.ceil(half / 2)){
+      newArray[i] = inputArray[i - 1];
     }
   }
   return newArray;
-}
+};
 
 //as of now I have not tested.  due to my accident I haven't done lab-01, I will complete that lab before doing tests here.  I will submit for now so That I have possibility of full credit or if not full credit willa t least maintain work flow.  Cheers
