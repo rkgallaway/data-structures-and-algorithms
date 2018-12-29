180cfb7ce154696f634862765e64f49111d53090
rkgallaway <rkgallaway@msn.com>
2018-12-28 18:07:55 -0800

    challenge 5 and 6 code working, needs tests

--- linkedList/linked-list.js
+++ linkedList/linked-list.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const util = require('util');
+
 //create node class
 class Node{
   constructor(value){
@@ -12,16 +14,53 @@ class Node{
 class LinkedList{
   constructor(){
     this.head = null;
-    this.next = null; // point to next??? or so the following methods handle that?
-  };
-  //define method insert here?
-  //define method includes here?
-  //define method print here?
+  }
+  add(value){
+    let node = new Node(value);
 
-  //still need to finish linked listts, but wanted current assignment added
+    if(!this.head){
+      this.head = node;
+      return;
+    }
+
+    let current = this.head;
+    while(current.next){
+      //do stuff
+      current = current.next;
+    }
+    current.next = node;
+  }
+
+  insert(value){
+    let node = new Node(value);
+    node.next = this.head;
+    this.head = node;
+  }
+
+  includes(value){
+    let current = this.head;
+    // let value = current.value;
+    while(current.next){
+      if(current.value === value){
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+  
+
+  print(){
+    let current = this.head;
+    while(current.next){
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+  //code challenge 06 begins
   append(value){
-    current = this.head;
-    while(current.next !== null){
+    let current = this.head;
+    while(current.next){
       current = current.next;
     };
     let newNode = new Node(value);
@@ -30,28 +69,63 @@ class LinkedList{
   };
 
   insertBefore(value, newVal){
-    current = this.head;
-    while(current.next !== null){
+    let current = this.head;
+    while(current.next){
       if(current.next.value === value){
         break;
       };
+      current = current.next;
+    }
+    if(current.next === null){
+      console.log('value does not exist');
+    }
+    else{
       let newNode = new Node(newVal);
       newNode.next = current.next;
       current.next = newNode;
     };
-    else (console.log('value does not exist'));
   };
 
-  insertAfter(vslue, newVal){
-    current = this.head;
-    while(current.next !== null){
+  insertAfter(value, newVal){
+    let current = this.head;
+    while(current.next){
       if(current.value === value){
         break;
       };
+      current = current.next;
+    };
+    if(current.next === null){
+      console.log('value does not exist');
+    }
+    else{
       let newNode = new Node(newVal);
       newNode.next = current.next;
       current.next = newNode;
     };
-    else (console.log('value does not exist'));
   };
+
+  kthFromEnd(k){
+    let arrayOfValues = [];
+    let current = this.head;
+    //edge cases?
+    while(current.next){
+      arrayOfValues.push(current.value);
+      current = current.next;
+    };
+    return arrayOfValues[arrayOfValues.length - (k + 1)];
+  }
 };
+
+let list = new LinkedList();
+list.add('Ryan');
+list.add('Amy');
+list.add('Wiener');
+list.add('Fat Alice');
+list .add('Scamper');
+list.insert('mom');
+console.log(list.includes('Amy'));
+list.insertBefore('Amy', 42);
+list.append('Shana');
+list.insertAfter('Wiener', 'dog');
+list.print();
+console.log(list.kthFromEnd(3));
