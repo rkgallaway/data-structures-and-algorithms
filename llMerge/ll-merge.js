dcc010d6d0d8ab694ca01535c5c8cf5edc499d05
rkgallaway <rkgallaway@msn.com>
2019-01-04 23:36:30 -0800

    updates to code challenge 08

--- llMerge/ll-merge.js
+++ llMerge/ll-merge.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const LinkedList = require('../linkedList.js');
+const list = require('../linkedList/linked-list.js');
 
-let list1 = new LinkedList();
+let list1 = new list.LinkedList();
 list1.add(1);
 list1.add(3);
 list1.add(5);
 
-let list2 = new LinkedList();
+let list2 = new list.LinkedList();
 list2.add(2);
 list2.add(4);
 list2.add(6);
@@ -15,21 +15,25 @@ list2.add(6);
 function zipList(list1, list2){
   let current1 = list1.head;
   let current2 = list2.head;
-  let zipCurrent = zipCurrent.next;
-  while(current1.next && current1){
-    zipCurrent.next = current1;
-    current1 = current1.next;
-    zipCurrent = zipCurrent.next;
-    zipCurrent.next = current2;
-    current2 = current2.next;
-    zipCurrent = zipCurrent.next;
+  let zippedList = new list.LinkedList();
+  while(current1.next || current2.next){
+    if(current1.value){
+      zippedList.add(current1.value);
+      current1 = current1.next;
+    } else
+    if(current2.value){
+      zippedList.add(current2.value);
+      current2 = current2.next;
+    }
   }
-  if(current1.next){
-    zipCurrent.next = current1;
-  } else {
-    zipCurrent.next = current2;
+  if(current1.value){
+    zippedList.add(current1.value);
   }
-  return zipList.head;
+  if(current2.value){
+    zippedList.add(current2.value);
+    return zippedList.head;
+  }
 }
 
 zipList();
+
