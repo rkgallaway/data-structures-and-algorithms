206b122e802a729471919564ec83362da750fd2b
rkgallaway <rkgallaway@msn.com>
2019-03-06 23:53:50 -0800

    301-10 started

--- 301/cc-11-regex-pt2/challenges-11.test.js
+++ 301/cc-11-regex-pt2/challenges-11.test.js
@@ -158,7 +158,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return the closing tags', () => {
     expect(findTagNames(['<h1>Hello, world!</h1>', '<p>Welcome to my site</p>'])).toStrictEqual([ '/h1', '/p' ]);
   });
