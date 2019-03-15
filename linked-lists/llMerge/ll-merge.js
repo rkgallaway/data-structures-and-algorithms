fc7d872936b1d3885d7cabf74d0bd538916195e2
rkgallaway <rkgallaway@msn.com>
2019-03-14 23:42:56 -0700

    ll-merge tests wired up

--- linked-lists/llMerge/ll-merge.js
+++ linked-lists/llMerge/ll-merge.js
@@ -1,21 +1,14 @@
 'use strict';
 
-const list = require('../linkedList/linked-list.js');
+const LinkedList = require('../linked-list.js');
 
-let list1 = new list.LinkedList();
-list1.add(1);
-list1.add(3);
-list1.add(5);
 
-let list2 = new list.LinkedList();
-list2.add(2);
-list2.add(4);
-list2.add(6);
 
 function zipList(list1, list2){
   let current1 = list1.head;
   let current2 = list2.head;
-  let zippedList = new list.LinkedList();
+  let zippedList = new LinkedList();
+
   while(current1.next || current2.next){
     if(current1.value){
       zippedList.add(current1.value);
@@ -35,5 +28,4 @@ function zipList(list1, list2){
   }
 }
 
-zipList();
-
+module.exports = zipList;
