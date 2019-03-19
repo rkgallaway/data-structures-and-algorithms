98f96ce16f750f0702251c789d9c69b3357aedd9
rkgallaway <rkgallaway@msn.com>
2019-03-18 23:18:58 -0700

    array reverse covered in tests

--- 401-code-challenges/arrayReverse/array-reverse.js
+++ 401-code-challenges/arrayReverse/array-reverse.js
@@ -4,12 +4,16 @@ function reverseArray(inArray){
   if(inArray.length <= 1){
     return inArray;
   }
+  if (Array.isArray(inArray) === false){
+    return 'please enter an array as a parameter';
+  }
   let newArray = [];
   let counter = 0;
   for (let i = inArray.length - 1; i >= 0; i--){
     newArray[counter] = inArray[i];
     counter++;
-  };
-  console.log(newArray);
+  }
   return newArray;
 }
+
+module.exports = reverseArray;
