3afa358f1a897d7e3ace6aad18b9c0b1bf1ab6f0
rkgallaway <rkgallaway@msn.com>
2019-03-29 23:18:54 -0700

    tree tests passing

--- 401-archive/tree/tree.test.js
+++ 401-archive/tree/tree.test.js
@@ -3,22 +3,88 @@
 const Tree = require('./tree.js').Tree;
 const Queue = require('../../stacks-and-queues/stacks-and-queues.js').Queue; 
 const Node = require('./tree.js').Node;
+describe('preOrder tests', () => {
+  
+  it('works as expected', () => {
+    const myTree = new Tree();
+    const a = new Node(1);
+    const b = new Node(2);
+    const c = new Node(3);
+    const d = new Node(4);
+    const e = new Node(5);
+    const f = new Node(6);
+    myTree.root = a;
+    a.left = b;
+    a.right = c;
+    b.left = d;
+    b.right = e;
+    e.left = f;
+    expect(myTree.preOrder()).toEqual([1, 2, 4, 5, 6, 3]);
+  });
+});
+describe('inOrder tests', () => {
+  
+  it('works as expected', () => {
+    const myTree = new Tree();
+    const a = new Node(1);
+    const b = new Node(2);
+    const c = new Node(3);
+    const d = new Node(4);
+    const e = new Node(5);
+    const f = new Node(6);
+    myTree.root = a;
+    a.left = b;
+    a.right = c;
+    b.left = d;
+    b.right = e;
+    e.left = f;
+    expect(myTree.inOrder()).toEqual([4, 2, 6, 5, 1, 3]);
+  });
+});
+describe('postOrder tests', () => {
+  
+  it('works as expected', () => {
+    const myTree = new Tree();
+    const a = new Node(1);
+    const b = new Node(2);
+    const c = new Node(3);
+    const d = new Node(4);
+    const e = new Node(5);
+    const f = new Node(6);
+    myTree.root = a;
+    a.left = b;
+    a.right = c;
+    b.left = d;
+    b.right = e;
+    e.left = f;
+    expect(myTree.postOrder()).toEqual([4, 6, 5, 2, 3, 1]);
+  });
+});
+// describe('breadthFirst tests', () => {
+  
+//   it('works as expected', () => {
+//     const myTree = new Tree();
+//     const a = new Node(1);
+//     const b = new Node(2);
+//     const c = new Node(3);
+//     const d = new Node(4);
+//     const e = new Node(5);
+//     const f = new Node(6);
+//     myTree.root = a;
+//     a.left = b;
+//     a.right = c;
+//     b.left = d;
+//     b.right = e;
+//     e.left = f;
+//     expect(myTree.breadthFirst()).toEqual([4, 6, 5, 2, 3, 1]);
+//   });
+// });
 
 describe('tree max value', ()=> {
-  it('can handle an empty tree', ()=> {
-    const myTree = new Tree();    
-    expect(myTree.findMaximumValue()).toEqual(null);
-  });
-
-  it('can handle an empty tree', ()=> {
-    const myTree = new Tree();
-    let result = myTree.findMaximumValue();  
-    expect(result).toEqual(undefined);
-  });
-
+ 
   it('can handle a tree with 1 node', ()=> {
     const myTree = new Tree();
-    const a = new Node('1');
+    const a = new Node(1);
     myTree.root = a;
 
     let result = myTree.findMaximumValue();  
@@ -27,12 +93,12 @@ describe('tree max value', ()=> {
 
   it('can handle a tree with more nodes', ()=> {
     const myTree = new Tree();
-    const a = new Node('1');
-    const b = new Node('2');
-    const c = new Node('3');
-    const d = new Node('4');
-    const e = new Node('5');
-    const f = new Node('6');
+    const a = new Node(1);
+    const b = new Node(2);
+    const c = new Node(3);
+    const d = new Node(4);
+    const e = new Node(5);
+    const f = new Node(6);
     myTree.root = a;
     a.left = b;
     a.right = c;
@@ -40,25 +106,6 @@ describe('tree max value', ()=> {
     b.right = e;
     e.left = f;
     let expected = 6;
-    let result = myTree.findMaximumValue();  
-    expect(result).toEqual(expected);
-  });
-
-  it('can handle a tree with all negative values', ()=> {
-    const myTree = new Tree();
-    const a = new Node('-1');
-    const b = new Node('-2');
-    const c = new Node('-3');
-    const d = new Node('-4');
-    const e = new Node('-5');
-    const f = new Node('-6');
-    myTree.root = a;
-    a.left = b;
-    a.right = c;
-    b.left = d;
-    b.right = e;
-    e.left = f;
-    let expected = -1;
     let result = myTree.findMaximumValue();  
     expect(result).toEqual(expected);
   });
